5499b2c9656781a89dfc876b266e137b8b766463
tetent888 <oakkyj@gmail.com>
2026-05-14 21:44:15 +0700

    add main.c

--- main.c
+++ main.c
@@ -1,20 +1,36 @@
 /*
- * main.c - Interactive menu that uses playlist, queue, and stack modules
+ * main.c - Demo program for CSV + playlist + queue + stack
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "csv.h"
 #include "include/playlist.h"
 #include "include/queue.h"
 #include "include/stack.h"
+
+#define CSV_FILE "songs.csv"
+#define MAX_SONGS 500
 
 static void freeSongCopy(Song *song) {
     free(song);
 }
 
+static void savePlaylistToCsv(Song *playlist) {
+    Song songs[MAX_SONGS];
+    int count;
+
+    count = playlistToArray(playlist, songs, MAX_SONGS);
+    if (csvSaveSongs(CSV_FILE, songs, count)) {
+        printf("Saved songs to %s\n", CSV_FILE);
+    } else {
+        printf("Cannot save songs to %s\n", CSV_FILE);
+    }
+}
+
 static void printPlayer(Song *nowPlaying, Queue *playQueue) {
-    Song *nextSong = peekQueue(playQueue);
+    Song *nextSong = queuePeek(playQueue);
 
     printf("\n");
     if (nowPlaying == NULL) {
@@ -23,7 +39,8 @@ static void printPlayer(Song *nowPlaying, Queue *playQueue) {
     } else {
         printf("  Now Playing : %s - %s\n", nowPlaying->title, nowPlaying->artist);
         printf("  Duration    : %d:%02d             Shuffle: OFF\n",
-               nowPlaying->duration / 60, nowPlaying->duration % 60);
+               nowPlaying->duration_seconds / 60,
+               nowPlaying->duration_seconds % 60);
     }
 
     printf("\n");
@@ -39,117 +56,143 @@ static void printPlayer(Song *nowPlaying, Queue *playQueue) {
 }
 
 int main(void) {
+    Song songs[MAX_SONGS];
+    Song newSong;
     Song *playlist = NULL;
     Queue playQueue;
     Stack history;
     Song *nowPlaying = NULL;
-    int choice, id, dur, dir;
-    char title[100], artist[100];
-    static int nextId = 1;
+    Song *found;
+    int songCount;
+    int choice;
+    char id[20];
+    char title[100];
+    char artist[100];
+    char genre[50];
+    int duration_seconds;
 
-    initQueue(&playQueue);
-    initStack(&history);
+    queueInit(&playQueue);
+    stackInit(&history);
+
+    songCount = csvLoadSongs(CSV_FILE, songs, MAX_SONGS);
+    playlist = playlistFromArray(songs, songCount);
 
     printf("\n=== Playlist Manager ===\n");
+    printf("Loaded %d songs from %s\n", songCount, CSV_FILE);
 
     while (1) {
         printPlayer(nowPlaying, &playQueue);
 
-        printf("\n1) Add to end        2) Add to front      3) Delete\n");
-        printf("4) Move up/down      5) Show playlist     6) Add to queue\n");
-        printf("7) Play next         8) Previous song     9) Show queue\n");
-        printf("10) Show history\n");
-        printf("0) Exit\n");
+        printf("\n1) Add song          2) Delete song       3) Search song\n");
+        printf("4) Show playlist     5) Add to queue      6) Play next\n");
+        printf("7) Previous song     8) Show queue        9) Show history\n");
+        printf("0) Save and exit\n");
         printf("Choice: ");
         if (scanf("%d", &choice) != 1) break;
 
         switch (choice) {
             case 1:
-                printf("Title: "); scanf(" %99[^\n]", title);
-                printf("Artist: "); scanf(" %99[^\n]", artist);
-                printf("Duration (seconds): "); scanf("%d", &dur);
-                playlist = addToEnd(playlist, nextId++, title, artist, dur);
+                printf("ID: ");
+                scanf(" %19[^\n]", id);
+                printf("Title: ");
+                scanf(" %99[^\n]", title);
+                printf("Artist: ");
+                scanf(" %99[^\n]", artist);
+                printf("Genre: ");
+                scanf(" %49[^\n]", genre);
+                printf("Duration (seconds): ");
+                scanf("%d", &duration_seconds);
+
+                songInit(&newSong, id, title, artist, genre, duration_seconds);
+                playlist = playlistAddEnd(playlist, &newSong);
+                printf("Added: %s - %s\n", title, artist);
                 break;
 
             case 2:
-                printf("Title: "); scanf(" %99[^\n]", title);
-                printf("Artist: "); scanf(" %99[^\n]", artist);
-                printf("Duration (seconds): "); scanf("%d", &dur);
-                playlist = addToFront(playlist, nextId++, title, artist, dur);
+                playlistShow(playlist);
+                printf("Song ID to delete: ");
+                scanf(" %19[^\n]", id);
+                playlist = playlistDeleteById(playlist, id);
                 break;
 
             case 3:
-                printf("Song ID to delete: "); scanf("%d", &id);
-                playlist = deleteSong(playlist, id);
+                printf("Search title: ");
+                scanf(" %99[^\n]", title);
+                found = playlistFindByTitle(playlist, title);
+                if (found == NULL) {
+                    printf("Song not found.\n");
+                } else {
+                    printf("Found: [%s] %s - %s (%s, %d:%02d)\n",
+                           found->id, found->title, found->artist, found->genre,
+                           found->duration_seconds / 60,
+                           found->duration_seconds % 60);
+                }
                 break;
 
             case 4:
-                printf("Song ID: "); scanf("%d", &id);
-                printf("Direction (-1=up, +1=down): "); scanf("%d", &dir);
-                playlist = moveSong(playlist, id, dir);
+                playlistShow(playlist);
                 break;
 
             case 5:
-                showPlaylist(playlist);
+                playlistShow(playlist);
+                printf("Song ID to add to queue: ");
+                scanf(" %19[^\n]", id);
+
+                found = playlistFindById(playlist, id);
+                if (found == NULL) {
+                    printf("Song ID %s not found.\n", id);
+                } else {
+                    queueEnqueue(&playQueue, found);
+                    printf("Added to queue: %s\n", found->title);
+                }
                 break;
 
             case 6: {
-                Song *found;
-
-                showPlaylist(playlist);
-                printf("Song ID to add to queue: ");
-                scanf("%d", &id);
-
-                found = findSongById(playlist, id);
-                if (found == NULL) {
-                    printf("[queue] Song ID %d not found.\n", id);
+                Song *next = queueDequeue(&playQueue);
+                if (next == NULL) {
+                    printf("Queue is empty.\n");
                 } else {
-                    enqueueSong(&playQueue, found);
+                    if (nowPlaying != NULL) {
+                        stackPush(&history, nowPlaying);
+                        freeSongCopy(nowPlaying);
+                    }
+                    nowPlaying = next;
+                    printf("Now playing: %s - %s\n",
+                           nowPlaying->title, nowPlaying->artist);
                 }
                 break;
             }
 
             case 7: {
-                Song *next = dequeueSong(&playQueue);
-                if (next != NULL) {
+                Song *previous = stackPop(&history);
+                if (previous == NULL) {
+                    printf("History is empty.\n");
+                } else {
                     if (nowPlaying != NULL) {
-                        pushSong(&history, nowPlaying);
-                        freeSongCopy(nowPlaying);
-                    }
-                    nowPlaying = next;
-                    printf("[play] Now playing \"%s\" - %s\n",
-                           nowPlaying->title, nowPlaying->artist);
-                }
-                break;
-            }
-
-            case 8: {
-                Song *previous = popSong(&history);
-                if (previous != NULL) {
-                    if (nowPlaying != NULL) {
-                        enqueueSong(&playQueue, nowPlaying);
+                        queueEnqueue(&playQueue, nowPlaying);
                         freeSongCopy(nowPlaying);
                     }
                     nowPlaying = previous;
-                    printf("[play] Back to \"%s\" - %s\n",
+                    printf("Back to: %s - %s\n",
                            nowPlaying->title, nowPlaying->artist);
                 }
                 break;
             }
 
-            case 9:
-                showQueue(&playQueue);
+            case 8:
+                queueShow(&playQueue);
                 break;
 
-            case 10:
-                showStack(&history);
+            case 9:
+                stackShow(&history);
                 break;
 
             case 0:
+                savePlaylistToCsv(playlist);
                 freeSongCopy(nowPlaying);
-                clearQueue(&playQueue);
-                clearStack(&history);
-                freePlaylist(playlist);
+                queueClear(&playQueue);
+                stackClear(&history);
+                playlistFree(playlist);
                 printf("Goodbye!\n");
                 return 0;
 
@@ -158,9 +201,10 @@ int main(void) {
         }
     }
 
+    savePlaylistToCsv(playlist);
     freeSongCopy(nowPlaying);
-    clearQueue(&playQueue);
-    clearStack(&history);
-    freePlaylist(playlist);
+    queueClear(&playQueue);
+    stackClear(&history);
+    playlistFree(playlist);
     return 0;
 }
